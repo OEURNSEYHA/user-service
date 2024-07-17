8b8028db3e996a485260bac5a73190d2a9a6fcb3
unknown <seyhaoeurn920@gmail.com>
2024-07-17 15:52:00 +0700

    update

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,7 +7,7 @@ import {
   Path,
   Queries,
   Request,
-  Middlewares,
+  // Middlewares,
   Put,
   Delete,
  
@@ -22,7 +22,7 @@ import {
 } from "@/src/utils/errors/customErrors";
 import { ReasonPhrases } from "@/src/utils/constands/satusCodes";
 import { UserService } from "../services/user.service";
-import {  authorizeUser } from "../middlewares/authRequire";
+// import {  authorizeUser } from "../middlewares/authRequire";
 
 export interface UserQueryParams {
   page?: number | undefined;
@@ -48,7 +48,7 @@ export class UsersController extends Controller {
 
   // get all user
   @Get()
-  @Middlewares([authorizeUser])
+  // @Middlewares([authorizeUser])
   async getAllUser(
     @Queries() query: UserQueryParams = { page: 1, limit: 10 }
   ): Promise<{ users: User[], totalUser: number, totalPages: number, }> {

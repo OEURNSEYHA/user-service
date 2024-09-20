758eac90043511c16173123d572373645fffc786
unknown <seyhaoeurn920@gmail.com>
2024-09-20 21:23:35 +0700

    update

--- build-script.js
+++ build-script.js
@@ -18,15 +18,15 @@ esbuild.build({
     copy({
       assets: [
         {
-          from: `node_modules/swagger-ui-dist/*.css`,
+          from: `../../../node_modules/swagger-ui-dist/*.css`,
           to: './',
         },
         {
-          from: `node_modules/swagger-ui-dist/*.js`,
+          from: `../../../node_modules/swagger-ui-dist/*.js`,
           to: './',
         },
         {
-          from: `node_modules/swagger-ui-dist/*.png`,
+          from: `../../../node_modules/swagger-ui-dist/*.png`,
           to: './',
         },
         {
@@ -47,7 +47,7 @@ esbuild.build({
   // (1) Solve: Copy swagger.json after successful build
   fs.copySync(path.resolve(__dirname, 'src/docs/swagger.json'), path.resolve(__dirname, 'build/docs/swagger.json'));
   console.log('Swagger JSON copied successfully!');
-
+  
   // Copy package.json after ensuring the build was successful
   fs.copySync(path.resolve(__dirname, 'package.json'), path.resolve(__dirname, 'build/package.json'));
   console.log('Package.json copied successfully!');
